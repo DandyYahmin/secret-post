0327ca418b1b0c2944baf4a80e67a47c692d1ac5
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-24 22:24:34 +0700

    'update'

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -16,4 +16,10 @@ class Thread extends Model
     public function comment_reply() {
         return $this->hasMany(Comment_reply::class);
     }
+
+    public function scopeFillter($query, array $fillters) {
+        $query->when($fillters['search'] ?? false, function ($query, $search) {
+            return $query->where('thread', 'like', '%'.$search.'%');
+        });
+    }
 }
