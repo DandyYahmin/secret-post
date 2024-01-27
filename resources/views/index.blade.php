a2f5f42b9eec924f62457da15ab7287689017717
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-27 22:12:58 +0700

    tailwind depedencies

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<table>
-    <form action="/1/2/reply" method="post" enctype="multipart/form-data">
-        @csrf
-        <input type="text" name="replies" id="">
-        <button type="submit">submit</button>
-    </form>
-    <h3>data:</h3>
+@extends('layouts.main')
+@section('container')
+<div class='pt-40'>
     <p>{{$threads}}</p>
-    <hr>
-    @foreach ($threads as $thread)
-    <p>isi thread: {{$thread->thread}}</p>
-    <p>tanggal dibuat thread: {{$thread->created_at}}</p>
-    <p>comment:</p>
-    @foreach ($thread->comment as $comment)
-    <p>{{$comment->comment}}</p>
-    @foreach ($comment->comment_reply as $reply)
-    <p>{{$reply->replies}}</p>
-    @endforeach
-    @endforeach
-    @endforeach
-</table>
-</body>
-</html>
+</div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Secret Post</title>
+</head>
+<body>
+    <div class='flex min-h-screen flex-col bg-white sm:items-center'>
+    @include('components.navbar')
+        <div class='container mx-auto px-12 py-4 lg:px-20'>    
+        @yield('container')
+        </div>
+    </div>
+</body>
+</html>
