b7a0645c153264778318661581e4de7a55ade9a2
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-27 22:11:44 +0700

    navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<div class='fixed sm:w-{40rem} px-8 justify-between top-0 right-0 left-0 z-10 bg-white p-5 items-center sm:shadow-xl shadow-lg flex flex-wrap'>
+    <p class='lg:text-6xl sm:text-2xl md:text-4xl text-xl font-mono'>👁‍🗨Secret-Post</p>
+    <form action="/" method="get" class=''>
+        <input name="search" type="text" placeholder="search any thread🔎" class='rounded-3xl shrink-0 border-2 border-current px-4 w-48 sm:h-12 sm:w-fit'>
+    </form>
+</div>
