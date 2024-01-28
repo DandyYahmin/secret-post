c968c9cb3680af35aed859e2306d0919739bd03e
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-28 21:47:41 +0700

    inserting thread component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('container')
-<div class='pt-40'>
-    <p>{{$threads}}</p>
+<div class='pt-36'>
+    @include('components.thread')
 </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     @vite('resources/css/app.css')
     <title>Secret Post</title>
 </head>
-<body>
+<body class='font-mono'>
     <div class='flex min-h-screen flex-col bg-white sm:items-center'>
     @include('components.navbar')
         <div class='container mx-auto px-12 py-4 lg:px-20'>    
