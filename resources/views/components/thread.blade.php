2215da62a134998e6675bc47758a8c9e11336c24
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-29 17:53:47 +0700

    fix comment button

--- resources/views/components/thread.blade.php
+++ resources/views/components/thread.blade.php
@@ -2,9 +2,10 @@
     @foreach ($threads as $thread)
     <div class='col-span-3 place-self-center text-left w-full h-fit p-3 pl-4 border border-current rounded-3xl'>
         <p class='text-amber-500 underline underline-offset-2'>Posted on {{$thread->created_at}}</p>
-        <p>{{$thread->thread}}</p>
+        <p class='mt-1'>{{$thread->thread}}</p>
         <br><br>
-        <p class='border border-current w-fit rounded-full p-1 text-sm bg-teal-500'>💬</p>
+        <a href="#{{$thread->created_at}}" class='border border-current w-fit rounded-full p-1 text-sm bg-teal-500'>💬</a>
     </div>
     @endforeach
+    {{$threads}}
 </div>
