4e5351f71a566967c97c3ee930fc77560ea1fe31
unknown <dandydarmawanalyahmin@gmail.com>
2024-01-30 20:42:41 +0700

    small update

--- resources/views/components/thread.blade.php
+++ resources/views/components/thread.blade.php
@@ -1,11 +1,14 @@
 <div class='grid grid-cols-1 gap-3'>
-    @foreach ($threads as $thread)
-    <div class='col-span-3 place-self-center text-left w-full h-fit p-3 pl-4 border border-current rounded-3xl'>
-        <p class='text-amber-500 underline underline-offset-2'>Posted on {{$thread->created_at}}</p>
-        <p class='mt-1'>{{$thread->thread}}</p>
-        <br><br>
-        <a href="#{{$thread->created_at}}" class='border border-current w-fit rounded-full p-1 text-sm bg-teal-500'>💬</a>
-    </div>
-    @endforeach
-    {{$threads}}
+    @if ($threads->count())
+        @foreach ($threads as $thread)
+        <div class='col-span-3 place-self-center text-left w-full h-fit p-3 pl-4 border border-current rounded-3xl'>
+            <p class='text-amber-500 underline underline-offset-2'>Posted on {{$thread->created_at}}</p>
+            <p class='mt-1'>{{$thread->thread}}</p>
+            <br><br>
+            <a href="#{{$thread->created_at}}" class='border border-current w-fit rounded-full p-1 text-sm bg-teal-500'>💬</a>
+        </div>
+        @endforeach
+    @else
+        <p class='text-center mt-10 text-red-600 text-2xl underline underline-offset-2'>No threads❓ try other keyword❗</p>
+    @endif
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,13 +6,14 @@
     @vite('resources/css/app.css')
     <title>Secret Post</title>
 </head>
-<body class='font-mono overflow-x-hidden'>
+<body class='font-mono'>
     <div class='flex min-h-screen flex-col bg-white sm:items-center'>
     @include('components.navbar')
         <div class='container mx-auto px-12 py-4 lg:px-80'>    
         @yield('container')
         @include('components.comment')
         </div>
+    @include('components.post')
     </div>
 </body>
 </html>
