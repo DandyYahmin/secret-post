7fa75cb209f558a9a24e18057b8c06583320ae1c
unknown <dandydarmawanalyahmin@gmail.com>
2024-02-14 22:22:11 +0700

    host update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <title>Secret Post</title>
+    <!-- backup -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class='font-mono'>
     @include('components.navbar')
